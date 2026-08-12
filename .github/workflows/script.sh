0deb54afdb441540ff61dd62357243b889139129
balajigajendran <48565693+balajigajendran@users.noreply.github.com>
2026-08-11 19:16:32 -0500

    udpated-script

--- .github/workflows/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt get install cowsay -y
-cowsay -f tiger "Run for cover, i m tiger .....RAWR" >> tiger.txt
-grep -i tiger.txt
-cat tiger.txt
-ls -lrt
--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt get install cowsay -y
+sudo apt-get install cowsay -y
 cowsay -f tiger "Run for cover, i m tiger .....RAWR" >> tiger.txt
 grep -i tiger.txt
 cat tiger.txt
